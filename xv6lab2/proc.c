f05a6c4dab14e86cd512b4262a15bf0625a1b227
Henry Ho <hho021@sledge.cs.ucr.edu>
2021-05-12 00:13:27 -0700

    Added requirements needed for new system call setprior

--- xv6lab2/proc.c
+++ xv6lab2/proc.c
@@ -322,37 +322,42 @@ wait(void)
 void
 scheduler(void)
 {
-  struct proc *p;
-  struct cpu *c = mycpu();
-  c->proc = 0;
-  
-  for(;;){
-    // Enable interrupts on this processor.
-    sti();
+    //implementing priority scheduler
+    struct proc *p;
+    struct cpu *c = mycpu();
 
-    // Loop over process table looking for process to run.
-    acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
-
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
-
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
-
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
-    }
-    release(&ptable.lock);
-
-  }
+//ROUND ROBIN SCHEDULER
+//  struct proc *p;
+//  struct cpu *c = mycpu();
+//  c->proc = 0;
+//
+//  for(;;){
+//    // Enable interrupts on this processor.
+//    sti();
+//
+//    // Loop over process table looking for process to run.
+//    acquire(&ptable.lock);
+//    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+//      if(p->state != RUNNABLE)
+//        continue;
+//
+//      // Switch to chosen process.  It is the process's job
+//      // to release ptable.lock and then reacquire it
+//      // before jumping back to us.
+//      c->proc = p;
+//      switchuvm(p);
+//      p->state = RUNNING;
+//
+//      swtch(&(c->scheduler), p->context);
+//      switchkvm();
+//
+//      // Process is done running for now.
+//      // It should have changed its p->state before coming back.
+//      c->proc = 0;
+//    }
+//    release(&ptable.lock);
+//
+//  }
 }
 
 // Enter scheduler.  Must hold only ptable.lock
@@ -531,4 +536,13 @@ procdump(void)
     }
     cprintf("\n");
   }
+}
+//MOD
+int setprior(int prior) {
+    struct proc *currproc = myproc(); //points to current process
+    acquire(&ptable.lock);
+    currproc->prior_val = prior;
+    yield();
+    sched();
+
 }
